6521b22e7d436808521d6d2bc077f1f4b574a712
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2022-08-09 15:33:37 +0530

    making all elements equal

---
+++ Hash Table/Problems/MakingAllEleEqu.cpp
@@ -0,0 +1,42 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+void MinDeleteOp(vector<int> v)
+{
+    unordered_map<int,int> m;
+    for(int i=0;i<v.size();i++)
+    {
+        //storing the frequency of the elements
+        m[v[i]]++;
+    }
+
+    int maxFre = 0;
+    for(auto temp : m)
+    {
+        if(temp.second>maxFre)
+        {
+            maxFre = temp.second;
+        }
+    }
+
+    cout<<"Minimum operations required to make all elements of the array equal : "<<v.size() - maxFre<<endl;
+}
+
+int main()
+{
+    int n,temp;
+    cin>>n;
+
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>temp;
+        v.push_back(temp);
+    }
+
+    MinDeleteOp(v);
+    return 0;
+}
--- README.md
+++ README.md
@@ -57,3 +57,5 @@ This repository contains implementation of different data structures and their p
 - Find whether an array is a subset of another array
 - Union and Intersection of two linked list.
 - Given an array A[] and a number x, check for pair in A[] with sum as x
+- Minimum delete operations to make all elements of array same.
+- Minimum operations to make all elements equal in array.
