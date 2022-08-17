367d3ad650f616bf1c8eaef336113626dea213a4
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2022-08-17 11:48:46 +0530

    Update ReverseString.cpp

--- Stack/Basic Programs/ReverseString.cpp
+++ Stack/Basic Programs/ReverseString.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 int main()
 {
+    //definning the string
     string str;
+    //getting the input
     cin>>str;
 
+    //initializing the stack
     stack<char>  s;
+    //pushing all the characters into the stack
     for(int i=0;i<str.length();i++)
     {
         s.push(str[i]);
     }
 
+    //printing all the characters stored inside the stack
     cout<<"Reverse of the string is : ";
     while(!s.empty())
     {
