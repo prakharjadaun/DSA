a1e26bcd55e140a4ea3d38dc81e15213167ed15e
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2022-08-23 10:54:55 +0530

    Update Fibonacci.cpp

--- Recursion/Basic Problems/Fibonacci.cpp
+++ Recursion/Basic Problems/Fibonacci.cpp
@@ -1,17 +1,27 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-void Fibonacci(int n)
+
+void Fib(int n)
 {
-    
+    static int pre=0,nex=1,res;
+    if (n>0)
+    {
+        res = pre + nex;
+        pre = nex;
+        nex = res;
+        cout << res << endl;
+        Fib(n-1);
+    }
 }
 
 int main()
 {
-    //declaring a variable n
+    // declaring a variable n
     int n;
-    //getting the input n
-    cout<<"Enter a number upto which you want to print the fibonacci series : ";
-    cin>>n;
-    Fibonacci(n);
+    // getting the input n
+    cout << "Enter a number upto which you want to print the fibonacci series : ";
+    cin >> n;
+    cout<<0<<endl<<1<<endl;
+    Fib(n-2);
     return 0;
 }
