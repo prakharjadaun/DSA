8f638726f8baa986b79c321603b0ce6f295de9e0
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2023-01-29 10:03:13 +0530

    file added : single number II

---
+++ 11 - Bitwise/SingleNumberII.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        //first sorting the array
+        sort(nums.begin(),nums.end());
+        int ans = 0;
+        for(int i=0;i<nums.size();)
+        {
+            if(i!=nums.size()-1)
+            {
+                //if a number is equal to next element
+                if(nums[i]==nums[i+1])
+                {
+                    //performing xor with that number here first then again below so that number which is 3 times can be xor as 0 and the number with single frequency remains in the answer variable
+                    ans = ans ^ nums[i];
+                    i+=2;
+                }
+            }
+            ans = ans ^ nums[i];
+            i++;
+        }    
+        return ans;
+    }
+};
