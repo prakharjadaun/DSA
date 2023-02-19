5f793c5167dbb21f13d65202e6751562c92e775b
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2023-02-19 22:47:43 +0530

    file added: sort array by parity

---
+++ 01 - Arrays/Basic Programs/SortByParity.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) 
+    {
+        int i = 0, j = nums.size()-1;
+        while(i<j)
+        {
+            if(nums[j] % 2 !=0)
+            {
+                j--;
+            }
+            else if(nums[i]%2!=0 && nums[j]%2==0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            else if(nums[i]%2==0)
+            {
+                i++;
+            }
+        } 
+        return nums;   
+    }
+};
